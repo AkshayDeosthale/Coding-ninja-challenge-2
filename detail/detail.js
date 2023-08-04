45880701abb81b600541b1f949cf33d27cf62207
AkshayDeosthale <akshay.deosthale6@gmail.com>
2023-08-04 11:18:13 +0530

    [ADD] Async data

--- detail/detail.js
+++ detail/detail.js
@@ -1,20 +1,40 @@
 //Using strict mode here to avoid errors
 "use strict";
 
-let activeMovie = JSON.parse(window.localStorage.getItem("activemovie"));
+let storage = JSON.parse(window.localStorage.getItem("activemovie"));
 
-document.addEventListener("DOMContentLoaded", function () {
+async function fetchByID() {
+  const res = await fetch(
+    `http://www.omdbapi.com/?i=${storage.imdbID}&apikey=e6b3e4b6`
+  );
+  const resjson = await res.json();
+  return resjson;
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const activeMovie = await fetchByID();
   //hero image
-  console.log(activeMovie);
   const heroImage = document.getElementById("movie-image");
   const heroName = document.getElementById("movie-title");
   const heroYear = document.getElementById("movie-type-year");
+  const plotHero = document.getElementById("plot");
+
+  const languageHero = document.getElementById("language");
+  const actorHero = document.getElementById("actors");
 
   function setCardDetails() {
     heroImage.src = activeMovie.Poster;
     heroName.innerText = activeMovie.Title;
-    heroYear.innerText = activeMovie.Type + " ," + activeMovie.Year;
+    heroYear.innerText = activeMovie.Genre + " ," + activeMovie.Year;
+    plotHero.innerText = activeMovie.Plot;
+
+    languageHero.innerText = activeMovie.Language;
+    actorHero.innerText = activeMovie.Actors;
   }
 
   setCardDetails();
+  const backbtn = document.getElementById("back");
+  backbtn.addEventListener("click", function () {
+    window.location.href = "/";
+  });
 });
--- script.js
+++ script.js
@@ -1,110 +1,41 @@
 //Using strict mode here to avoid errors
 "use strict";
-const movies = {
-  Hero: {
-    Title: "Rockwell Tools Bladerunner X2",
-    Year: "2015",
-    imdbID: "tt5855480",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMzM5MTcxMTgtODAwNS00OWNmLWFlYzYtN2QzNzZiMTQzNGZhXkEyXkFqcGdeQXVyNTY2NTE2MzA@._V1_SX300.jpg",
-  },
-
-  Search: [
-    {
-      Title: "Last Action Hero",
-      Year: "1993",
-      imdbID: "tt0107362",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNjdhOGY1OTktYWJkZC00OGY5LWJhY2QtZmQzZDA2MzY5MmNmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
-    },
-    {
-      Title: "Looney Tunes: Back in Action",
-      Year: "2003",
-      imdbID: "tt0318155",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZDc0NTc4YTMtOTRiYS00MzQ5LTg5MDAtYWMzZTM5MjljYWViXkEyXkFqcGdeQXVyMTUyOTc1NDYz._V1_SX300.jpg",
-    },
-    {
-      Title: "A Civil Action",
-      Year: "1998",
-      imdbID: "tt0120633",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZmEzNjhiZWEtNTM5OS00ZmQyLThhYjEtNjY4ZDZhOGFkMzI4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    },
-    {
-      Title: "An Action Hero",
-      Year: "2022",
-      imdbID: "tt15600222",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMDJiOTdmMGItMmM5MC00ZTRiLWIzNjctNDE4ZTZkMWMzZTg0XkEyXkFqcGdeQXVyOTI3MzI4MzA@._V1_SX300.jpg",
-    },
-    {
-      Title: "Missing in Action",
-      Year: "1984",
-      imdbID: "tt0087727",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNDczNDljZTUtNmZmZC00YzFkLWExYzEtYzQxNmNkNmFjMDQyXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg",
-    },
-    {
-      Title: "Action Point",
-      Year: "2018",
-      imdbID: "tt6495770",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjEyMTU5MTk1N15BMl5BanBnXkFtZTgwMzIzMzczNTM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Action Jackson",
-      Year: "1988",
-      imdbID: "tt0094612",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYjc0Y2E2ZjYtZGQxNi00NDgxLTk0OTctMDAzYTg1MzFmMjI4XkEyXkFqcGdeQXVyNjc5NjEzNA@@._V1_SX300.jpg",
-    },
-    {
-      Title: "321 Action",
-      Year: "2020",
-      imdbID: "tt13423846",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYTRmYzgyZjEtN2UyZS00NDYxLTlkMDctMGJjZjY0ODExNWE5XkEyXkFqcGdeQXVyNjI2ODk3NTM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Missing in Action 2: The Beginning",
-      Year: "1985",
-      imdbID: "tt0089604",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOTFhNTdiNDQtZGQ4Ny00MDA1LTg1ZjEtYzZhMGU5YjBjNTBhXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg",
-    },
-    {
-      Title: "Class Action",
-      Year: "1991",
-      imdbID: "tt0101590",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNWY5Mjk4ZmItMTAzYS00NWE3LWEzYzYtNDgzY2MwMzA3MDIzXkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_SX300.jpg",
-    },
-  ],
-  totalResults: "1262",
-  Response: "True",
-};
 
 let likedMovies = JSON.parse(window.localStorage.getItem("likedMovies")) || [];
 
-document.addEventListener("DOMContentLoaded", function () {
+let movies;
+
+async function fetchHeroData() {
+  const herores = await fetch(
+    "http://www.omdbapi.com/?i=tt3896198&apikey=e6b3e4b6"
+  );
+  const heroresJSON = await herores.json();
+
+  return heroresJSON;
+}
+
+async function fetchMovies(seachString = "action") {
+  const res = await fetch(
+    `http://www.omdbapi.com/?s=${seachString}&apikey=e6b3e4b6`
+  );
+  const resJSON = await res.json();
+  return resJSON.Search;
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const hero = await fetchHeroData();
+  const list = await fetchMovies();
+
+  movies = { Search: list, Hero: hero };
+
   //hero image
   const heroImage = document.getElementById("hero-image");
   const heroName = document.getElementById("hero-name");
   const heroYear = document.getElementById("hero-year");
   const explore = document.getElementById("explore-hero");
   const likeHero = document.getElementById("like-hero");
+  const plotHero = document.getElementById("plot");
+  const genreHero = document.getElementById("genre");
 
   //page header
   const headerLikeTemplate = document.getElementById("header-liked");
@@ -170,6 +101,8 @@ document.addEventListener("DOMContentLoaded", function () {
     heroImage.src = movies.Hero.Poster;
     heroName.innerText = movies.Hero.Title;
     heroYear.innerText = movies.Hero.Year;
+    plotHero.innerText = movies.Hero.Plot;
+    genreHero.innerText = movies.Hero.Genre;
   }
 
   setHerodetails();
